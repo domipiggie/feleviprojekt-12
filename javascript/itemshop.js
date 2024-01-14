5bba7fd78c2e5b2b51faa3f4f579d695b8155e88
domipiggie <domipiggie@gmail.com>
2024-01-14 17:53:44 +0100

    itemSection példányosítás szerver válaszból

--- javascript/itemshop.js
+++ javascript/itemshop.js
@@ -1,23 +1,28 @@
-class Item{
-    displayName; displayDescription; fullBackground; sectionName;
+const apiLink = "https://fortniteapi.io/v2/shop?lang=en"
+const xhr = new XMLHttpRequest()
 
-    constructor(displayName, displayDescription, fullBackground, sectionName){
-        this.displayName = displayName
-        this.displayDescription = displayDescription
-        this.fullBackground = fullBackground
-        this.sectionName = sectionName
+window.addEventListener("load", function(){
+    let sections = []
+
+    xhr.open("GET", apiLink)
+    xhr.onload = function(){
+        if (xhr.status != 200){
+            return
+        }
+
+        const respJson = JSON.parse(xhr.response)
+
+        respJson.shop.forEach(function(item, index){
+            for (i in sections){
+                if (sections[i].sectionName == item.section.name){
+                    return
+                }
+            }
+            sections.push(new Section(item.section.name))
+        })
+
+        console.log(sections)
     }
-}
-
-class Section{
-    sectionName;
-    items = []
-
-    addItem(item) {
-        this.items.push(item)
-    }
-
-    constructor(sectionName){
-        this.sectionName = sectionName
-    }
-}
+    xhr.setRequestHeader("Authorization", "d1853e9c-b4186298-76a2e520-146c9d4d")
+    xhr.send()
+})
