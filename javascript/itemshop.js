b7c1886606c1aaab18fcf73b94344e0522dc26c1
domipiggie <domipiggie@gmail.com>
2024-01-14 18:17:34 +0100

    basic item display

--- javascript/itemshop.js
+++ javascript/itemshop.js
@@ -1,5 +1,6 @@
 const apiLink = "https://fortniteapi.io/v2/shop?lang=en"
 const xhr = new XMLHttpRequest()
+const itemsDiv = document.getElementById("items")
 
 function makeItem(item){
     let backgrounds = []
@@ -9,8 +10,28 @@ function makeItem(item){
     return new Item(item.displayName, item.displayDescription, backgrounds, item.section.name)
 }
 
+function drawItem(item){
+    let mainDiv = document.createElement("div")
+    mainDiv.classList = "col-md-6 item"
+
+    let image = document.createElement("img")
+    image.src = item.fullBackgrounds[0]
+
+    let name = document.createElement("h3")
+    name.innerText = item.displayName
+
+    let desc = document.createElement("p")
+    desc.innerText = item.displayDescription
+
+    mainDiv.appendChild(image)
+    mainDiv.appendChild(name)
+    mainDiv.appendChild(desc)
+    itemsDiv.appendChild(mainDiv)
+}
+
 window.addEventListener("load", function(){
     let sections = []
+    let respJson;
 
     xhr.open("GET", apiLink)
     xhr.onload = function(){
@@ -18,7 +39,7 @@ window.addEventListener("load", function(){
             return
         }
 
-        const respJson = JSON.parse(xhr.response)
+        respJson = JSON.parse(xhr.response)
 
         respJson.shop.forEach(function(item, index){
             for (i in sections){
@@ -30,8 +51,14 @@ window.addEventListener("load", function(){
             sections.push(new Section(item.section.name))
             sections[sections.length-1].addItem(makeItem(item))
         })
-
+    
         console.log(sections)
+    
+        sections.forEach(function(i){
+            i.items.forEach(function(item){
+                drawItem(item)
+            })
+        })
     }
     xhr.setRequestHeader("Authorization", "d1853e9c-b4186298-76a2e520-146c9d4d")
     xhr.send()
