417764b235da0a17cd7966f04e59dbcbd605db89
domipiggie <domipiggie@gmail.com>
2024-01-14 17:59:33 +0100

    item class példányosítás minden tárgyra

--- javascript/itemshop.js
+++ javascript/itemshop.js
@@ -15,10 +15,12 @@ window.addEventListener("load", function(){
         respJson.shop.forEach(function(item, index){
             for (i in sections){
                 if (sections[i].sectionName == item.section.name){
+                    sections[i].addItem(new Item(item.displayName, item.displayDescription, item.granted[0].images.full_background, item.section.name))
                     return
                 }
             }
             sections.push(new Section(item.section.name))
+            sections[sections.length-1].addItem(new Item(item.displayName, item.displayDescription, item.granted[0].images.full_background, item.section.name))
         })
 
         console.log(sections)
